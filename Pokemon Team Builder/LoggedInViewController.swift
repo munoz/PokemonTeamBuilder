f5726e509b36d03b30b331d608beebab320b4143
Pedro Muñoz <pmunoz@csumb.edu>
2020-11-05 22:48:47 -0800

    Login and Sign Up configured and tested

---
+++ Pokemon Team Builder/LoggedInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoggedInViewController.swift
+//  Pokemon Team Builder
+//
+//  Created by studentProfile on 11/5/20.
+//  Copyright © 2020 munoz. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoggedInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
