faa443bdb52253dade744886e4ca8c8f5c259d62
Pedro Muñoz <pmunoz@csumb.edu>
2020-11-05 20:38:48 -0800

    Initial Commit

---
+++ Pokemon Team Builder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon Team Builder
+//
+//  Created by studentProfile on 11/5/20.
+//  Copyright © 2020 munoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
