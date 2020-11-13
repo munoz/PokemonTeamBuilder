1c56f11f00add433463d64db21c0734387606886
Eduardo445 <esoto-rodriguez@csumb.edu>
2020-11-13 15:04:04 -0800

    Creating branch

---
+++ Pokemon Team Builder/PokemonTeamsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTeamsTableViewCell.swift
+//  Pokemon Team Builder
+//
+//  Created by user180060 on 11/13/20.
+//  Copyright © 2020 munoz. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTeamsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
